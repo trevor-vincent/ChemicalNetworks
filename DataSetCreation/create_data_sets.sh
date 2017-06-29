369e54984e7e64ea58c7e63b21aff07cb2322460
Trevor Vincent <tvincent@cita.utoronto.ca>
2017-06-29 12:57:02 -0400

    Add Gunjan fixes for create_data_sets.sh, now working

--- DataSetCreation/create_data_sets.sh
+++ DataSetCreation/create_data_sets.sh
@@ -11,7 +11,7 @@ function write_submit {
 #PBS -N $3
 
 module purge;
-module load python;
+module load intel/15.0.2 python/2.7.8;
 cd ${1}
 
 time python $2 2>&1 | tee vulcan.out
@@ -43,8 +43,8 @@ EQ_ini_file = ''
 na = 4 # na: The number of elements. Default is 4: H,O,C,He
 atom_list = ['H', 'O', 'C', 'He']
 # default: solar abundance (from K.Lodders 2009)
-O_H = $1
-C_H = $2
+O_H = $1/$2
+C_H = $1
 He_H = 0.09691
 ini_mix = 'EQ' # 'EQ' or 'CH4'
 
@@ -113,9 +113,14 @@ use_PIL = True
 EOF
 }
 
-arr1=( 1e-5 1e-4 1e-3 1e-2 ) #O-H
-arr2=( 1e-5 1e-4 1e-3 1e-2 ) #C-H
-
+arr1=( 1e-5 1e-4 1e-3 1e-2  ) #C-H
+arr2=( 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.71 0.72 0.73 0.74 0.75 0.76 0.77 0.78 0.79 0.8 0.81
+ 0.82 0.83 0.84 0.85 0.86 0.87 0.88 0.89 0.9 1. 1.1  1.2  1.3  1.4  1.5  1.6  1.7  1.8  1.9  2.   2.1  2.2  2.3  2.4
+  2.5  2.6  2.7  2.8  2.9  3.   3.1  3.2  3.3  3.4  3.5  3.6  3.7  3.8  3.9
+  4.   4.1  4.2  4.3  4.4  4.5  4.6  4.7  4.8  4.9  5.   5.1  5.2  5.3  5.4
+  5.5  5.6  5.7  5.8  5.9  6.   6.1  6.2  6.3  6.4  6.5  6.6  6.7  6.8  6.9
+  7.   7.1  7.2  7.3  7.4  7.5  7.6  7.7  7.8  7.9  8.   8.1  8.2  8.3  8.4
+  8.5  8.6  8.7  8.8  8.9  9.   9.1  9.2  9.3  9.4  9.5  9.6  9.7  9.8  9.9 )
 if [ "$#" -ne 3 ]; then
     echo "Illegal number of parameters"
 fi
@@ -124,10 +129,10 @@ for a in "${arr1[@]}"
 do
     for b in "${arr2[@]}"
     do
-    NEWDIR="vulcan_OH_${a}_CH_${b}"
+    NEWDIR="vulcan_CH_${a}_CO_${b}"
     mkdir $NEWDIR
     cd $NEWDIR
-    SHORTNAME="OH_${a}_CH_${b}"
+    SHORTNAME="CH_${a}_CO_${b}"
     rundir=$PWD
     executable_path=$1
     cp
@@ -135,7 +140,7 @@ do
     cp -r "${executable_path}"/* .
     rm vulcan_cfg.py
     write_cfg $a $b
-    	# qsub submit.sh
+    qsub submit.sh
     cd ..
     done  
 done
